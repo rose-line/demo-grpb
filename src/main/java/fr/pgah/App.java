03ad8f33fd64557d2d4c7b1e74122d3b67c3396c
rose-line <1886792+rose-line@users.noreply.github.com>
2024-09-19 16:23:52 +0200

    first commit

---
+++ src/main/java/fr/pgah/App.java
@@ -0,0 +1,25 @@
+package fr.pgah;
+
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) {
+    // 0. Initialisation du Scanner
+    Scanner clavier = new Scanner(System.in); // initialisation du Scanner clavier
+
+    // 1. Récupérer salaire horaire
+    System.out.println("Entrez votre salaire horaire : ");
+    int salaireHoraire = clavier.nextInt();
+
+    // 2. Récupérer nb heures travaillées
+    System.out.println("Entrez vos heures travaillées : ");
+    int nbHeuresTravaillees = clavier.nextInt();
+
+    // 3. Multiplier
+    int resultat = salaireHoraire * nbHeuresTravaillees;
+
+    // 4. Afficher
+    System.out.println("Le salaire brut est : " + resultat);
+  }
+}
